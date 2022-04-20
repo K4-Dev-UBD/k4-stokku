86e2a1b0b467524fbec6f619799b193af0b3bb14
Verdo Fanv <verdo.dev@gmail.com>
2022-04-20 21:07:45 +0700

    initial project

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$username = "Admin" 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stokku - Home</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+<h1>Hello, <?= $username; ?></h1>
+
+</body>
+</html>
