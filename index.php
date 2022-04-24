0cb11679954dfad77c84a16ccc75c911a5954188
Verdo Fanv <verdo.dev@gmail.com>
2022-04-24 20:53:05 +0700

    add detail page, button and breadcrump

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 $username = "Admin";
+// var_dump($_SERVER['HTTP_HOST']);
+// var_dump($_SERVER['REQUEST_URI'])
 
 ?>
 
@@ -11,17 +13,25 @@ $username = "Admin";
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Stokku - Kelola semua barang dagang-Mu dengan simple dan cepat.">
+  <meta http-equiv="Cache-control" content="no-cache">
   <title>Stokku - Home</title>
-  <link rel="stylesheet" type="text/css" href="styles/style.css">
+  <link rel="stylesheet" type="text/css" href="./styles/style.css">
   <link rel="icon" href="public/icons/javascript-icon.ico">
   <link rel="apple-touch-icon" href="public/icons/javascript-icon.ico">
 </head>
 <body>
-  <header class="hero-head">
-    <h1>Stokku</h1>
-    <p>Manage your trade</p>
-  </header>
+  <header class="main-head">
+    <div class="breadcrump">
+      <p class="head-legend">Stokku</p>
+      <div class="head-link">
+        <a href="<?= $_SERVER['REQUEST_URI'] ?>" class="active">Home</a>
+      </div>
+    </div>
 
+    <div class="head-action">
+      <button class="button button-success">Tambah Barang</button>
+    </div>
+  </header>
   <div class="barang">
     <div class="barang-inner">
       <!-- <header class="barang-head">
@@ -37,12 +47,21 @@ $username = "Admin";
             </div>
           </div>
           <div class="card-body">
-            <a href="product.php?id=1" class="product-name">Product Name</a>
-            <p class="product-stock">Stock: 10</p>
+            <div class="row">
+              <a href="product.php?id=1" class="product-name">Product Name</a>
+            </div>
+            <div class="row">
+              <p>Stock: 10</p>
+            </div>
+            <div class="row">
+              <p class="text-muted text-bold">Rp 10000</p>
+            </div>
           </div>
           <div class="card-footer">
             <div class="card-action">
-              <button type="button" class="button button-delete">Delete</button>
+              <div class="row">
+                <button class="button button-primary button-view">View</button>
+              </div>
             </div>
           </div>
         </div>
--- product.php
+++ product.php
@@ -18,10 +18,31 @@ $id = $_GET["id"];
 </head>
 <body>
     
-  <header class="hero-head">
-    <h1>Stokku</h1>
-    <p>Manage your trade</p>
-  </header>
+  <form method="POST">
+    <header class="main-head">
+      <div class="breadcrump">
+        <p class="head-legend">Stokku</p>
+        <div class="head-link">
+          <a href="/">Home</a> / <a href="/" class="active">Detail</a>
+        </div>
+      </div>
+      <div class="head-action">
+        <div class="form-field-action">
+          <button class="button button-delete">Delete</button>
+          <button type="submit" class="button button-primary">Save</button>
+        </div>
+      </div>
+    </header>
+    <div class="form-field">
+      <div class="form-field-body">
+        <div class="row">
+          <div class="form-input">
+            <label for="name" class="form-input-heading">Name</label>
+            <input type="text" class="input" name="name" placeholder="Name input here">
+          </div>
+        </div>
+    </div>
+  </form>
 
 </body>
 </html>
