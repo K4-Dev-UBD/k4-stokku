c2d8937454af83e5b9d84c6331c0631ebb1a72a6
TSoos1304 <satriachristian@gmail.com>
2022-06-16 00:31:56 +0700

    k4stokku_db(1)
    masukan 3 produk ke k4stokku_db(1)
    ada di services

---
+++ services/mysql/k4stokku_db(1).sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 15, 2022 at 07:27 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `k4stokku_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `product_id` varchar(16) NOT NULL,
+  `product_name` text NOT NULL,
+  `stock` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `asal` text NOT NULL,
+  `jenis` text NOT NULL,
+  `expired` text NOT NULL,
+  `tanggal_beli` text NOT NULL,
+  `deskripsi` text NOT NULL,
+  `harga_jual` int(11) NOT NULL,
+  `created_date` text NOT NULL,
+  `updated_date` text NOT NULL,
+  `gambar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`product_id`, `product_name`, `stock`, `price`, `asal`, `jenis`, `expired`, `tanggal_beli`, `deskripsi`, `harga_jual`, `created_date`, `updated_date`, `gambar`) VALUES
+('Mk01', 'Good Times Cookies dus', 100, 1200, 'Indo Grosir', 'Snack', '31 December 2022', '1 June 2022', 'Merk : Good Times Cookies\r\n1 Box isi 12 pcs @15gr\r\n', 14000, '', '', 'https://cf.shopee.co.id/file/a6ca3cb7c6947cb965b47df717e50844'),
+('Mk02', 'Indomie Goreng dus', 100, 118000, 'Indo Grosir', 'Makanan', '31 December 2022', '1 June 2022', 'Merk : Indomie Goreng 1 dus 1 box isi 40 pcs', 125000, '', '', 'https://cf.shopee.co.id/file/159e7d81de24e32f07ede8aa4cb275cd'),
+('BM01', 'Minyak Goreng Sania 2 liter', 100, 45000, 'Indo Grosir', 'Bahan Masak', '31 December 2022', '1 June 2022', 'Merk : Sania\r\nBerat : 2 liter', 50000, '', '', 'https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/3/23/0e3b496c-1b6b-4c4d-aa8f-c34dc48824dc.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_barang`
+--
+
+CREATE TABLE `jenis_barang` (
+  `nama` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`nama`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
