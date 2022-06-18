8f28129df092127447b8717e12a1cbbe7e1a35f6
Stevenjryo <104193562+Stevenjryo@users.noreply.github.com>
2022-06-18 12:01:51 +0700

    ambil semua barang & tambah semua barang
    tambah code ,tambah barang & ambil barang di file barang.services

--- services/mysql/barangService.php
+++ services/mysql/barangService.php
@@ -1,11 +1,35 @@
 <?php 
 
-class BarangService {
+class BarangService 
+{
   public function __construct() {
-    private $this->db = new PDO();
+    private $this->db = new PDO('mysql:host=local host;dbname=barang','root','');
   }
 
   // write method for add & get
+
+
+    public function tambah_barang($product_id,$product_name,$stock,$price,$asal,$jenis,$expired,$tanggal_beli,$deskripsi,$harga_jual,$created_date,$updated_date,$gambar){
+      $sql= "INSERT INTO barang (product_id,product_name,stock,price,asal,jenis,expired,tanggal_beli,deskripsi,harga_jual,created_date,updated_date,gambar)VALUE ('$product_id','$product_name','$stock','$price','$asal','$jenis','$expired','$tanggal_beli','$deskripsi','$harga_jual','$created_date','$updated_date','$gambar')";
+      $query= $this->db->query($sql);
+      if(!$query)
+      {
+        return "Failed";
+      }
+      else
+      {
+        return "Success";
+      }
+
+    }
+  }
+  public function select_barang()
+  {
+    $sql="ambil_barang *  barang";
+    $query= $this->db->query($sql);
+    return $query;
+}
+?>
 }
 
 ?>
