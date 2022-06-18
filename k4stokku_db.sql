717874b761b41e19b09957688737041ff7dce6e9
TSoos1304 <satriachristian@gmail.com>
2022-06-18 22:39:07 +0700

    updated barangServices

--- k4stokku_db.sql
+++ k4stokku_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 15, 2022 at 07:27 PM
+-- Generation Time: Jun 18, 2022 at 04:58 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `barang` (
-  `product_id` varchar(16) NOT NULL,
+  `product_id` int(11) NOT NULL,
   `product_name` text NOT NULL,
   `stock` int(11) NOT NULL,
   `price` int(11) NOT NULL,
@@ -48,19 +48,29 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`product_id`, `product_name`, `stock`, `price`, `asal`, `jenis`, `expired`, `tanggal_beli`, `deskripsi`, `harga_jual`, `created_date`, `updated_date`, `gambar`) VALUES
-('Mk01', 'Good Times Cookies dus', 100, 1200, 'Indo Grosir', 'Snack', '31 December 2022', '1 June 2022', 'Merk : Good Times Cookies\r\n1 Box isi 12 pcs @15gr\r\n', 14000, '', '', 'https://cf.shopee.co.id/file/a6ca3cb7c6947cb965b47df717e50844'),
-('Mk02', 'Indomie Goreng dus', 100, 118000, 'Indo Grosir', 'Makanan', '31 December 2022', '1 June 2022', 'Merk : Indomie Goreng 1 dus 1 box isi 40 pcs', 125000, '', '', 'https://cf.shopee.co.id/file/159e7d81de24e32f07ede8aa4cb275cd'),
-('BM01', 'Minyak Goreng Sania 2 liter', 100, 45000, 'Indo Grosir', 'Bahan Masak', '31 December 2022', '1 June 2022', 'Merk : Sania\r\nBerat : 2 liter', 50000, '', '', 'https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/3/23/0e3b496c-1b6b-4c4d-aa8f-c34dc48824dc.jpg');
-
--- --------------------------------------------------------
+(1, 'Good Times Cookies dus', 100, 1200, 'Indo Grosir', 'Snack', '31 December 2022', '1 June 2022', 'Merk : Good Times Cookies\r\n1 Box isi 12 pcs @15gr\r\n', 14000, '', '', 'https://cf.shopee.co.id/file/a6ca3cb7c6947cb965b47df717e50844'),
+(2, 'Indomie Goreng dus', 100, 118000, 'Indo Grosir', 'Makanan', '31 December 2022', '1 June 2022', 'Merk : Indomie Goreng 1 dus 1 box isi 40 pcs', 125000, '', '', 'https://cf.shopee.co.id/file/159e7d81de24e32f07ede8aa4cb275cd'),
+(3, 'Minyak Goreng Sania 2 liter', 100, 45000, 'Indo Grosir', 'Bahan Masak', '31 December 2022', '1 June 2022', 'Merk : Sania\r\nBerat : 2 liter', 50000, '', '', 'https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/3/23/0e3b496c-1b6b-4c4d-aa8f-c34dc48824dc.jpg');
 
 --
--- Table structure for table `jenis_barang`
+-- Indexes for dumped tables
 --
 
-CREATE TABLE `jenis_barang` (
-  `nama` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`nama`))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- services/mysql/barangService.php
+++ services/mysql/barangService.php
@@ -5,8 +5,8 @@ class BarangService {
     private $this->db = new PDO('mysql:host=localhost host;dbname=barang', 'root', '');
   }
 
-  public function tambah_barang($product_id, $product_name, $stock, $price, $asal, $jenis, $expired, $tanggal_beli, $deskripsi, $harga_jual, $created_date, $updated_date, $gambar) {
-    $sql= "INSERT INTO barang (product_id, product_name, stock, price, asal, jenis, expired, tanggal_beli, deskripsi, harga_jual, created_date, updated_date, gambar)VALUES ('$product_id', '$product_name', '$stock', '$price', '$asal', '$jenis', '$expired', '$tanggal_beli', '$deskripsi', '$harga_jual', '$created_date', '$updated_date', '$gambar')";
+  public function tambah_barang($product_name, $stock, $price, $asal, $jenis, $expired, $tanggal_beli, $deskripsi, $harga_jual, $created_date, $updated_date, $gambar) {
+    $sql= "INSERT INTO barang (product_name, stock, price, asal, jenis, expired, tanggal_beli, deskripsi, harga_jual, created_date, updated_date, gambar)VALUES ('$product_name', '$stock', '$price', '$asal', '$jenis', '$expired', '$tanggal_beli', '$deskripsi', '$harga_jual', '$created_date', '$updated_date', '$gambar')";
     $query= $this->db->query($sql);
 
     if (!$query) {
@@ -17,17 +17,30 @@ class BarangService {
   }
 
   public function select_barang() {
-    $sql="ambil_barang *  barang";
+    $sql="SELECT * FROM barang";
     $query= $this->db->query($sql);
     return $query;
   }
 
-  public function edit_barang() {
-    $sql = "SELECT * FROM barang WHERE Product ID = '$product_id'";
+  public function edit_barang($product_id, $product_name, $stock, $price, $asal, $jenis, $expired, $tanggal_beli, $deskripsi, $harga_jual, $created_date, $updated_date, $gambar) {
+    $sql = "UPDATE barang SET 
+product_name = '$product_name', 
+stock = '$stock',
+price = '$price',
+asal = '$asal',
+jenis = '$jenis',
+expired = '$expired',
+tanggal_beli = '$tanggal_beli',
+deskripsi = '$deskripsi',
+harga_jual = '$harga_jual',
+created_date = '$created_date',
+updated_date = '$updated_date',
+gambar = '$gambar'
+WHERE product_id = '$product_id'";
   }
 
-  public function delete_barang($product ID) {
-    $sql = "DELETE FROM barang WHERE product ID = '$nim'";
+  public function delete_barang($product_id) {
+    $sql = "DELETE FROM barang WHERE product_id = '$product_id'";
     $query = $this->db->query($sql);
 
     if (!$query) {
