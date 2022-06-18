75782aa01761f2ec101542ae7226f2f8c00ead7d
Verdo Fanv <verdo.dev@gmail.com>
2022-06-18 20:40:08 +0700

    Update barangService.php

--- services/mysql/barangService.php
+++ services/mysql/barangService.php
@@ -1,46 +1,41 @@
 <?php 
 
-class BarangService 
-{
-  public function __construct() 
-  {
-    private $this->db = new PDO('mysql:host=local host;dbname=barang','root','');
+class BarangService {
+  public function __construct() {
+    private $this->db = new PDO('mysql:host=localhost host;dbname=barang', 'root', '');
   }
 
-  // write method for add & get
-  public function tambah_barang($product_id,$product_name,$stock,$price,$asal,$jenis,$expired,$tanggal_beli,$deskripsi,$harga_jual,$created_date,$updated_date,$gambar)
-  {
-    $sql= "INSERT INTO barang (product_id,product_name,stock,price,asal,jenis,expired,tanggal_beli,deskripsi,harga_jual,created_date,updated_date,gambar)VALUE ('$product_id','$product_name','$stock','$price','$asal','$jenis','$expired','$tanggal_beli','$deskripsi','$harga_jual','$created_date','$updated_date','$gambar')";
+  public function tambah_barang($product_id, $product_name, $stock, $price, $asal, $jenis, $expired, $tanggal_beli, $deskripsi, $harga_jual, $created_date, $updated_date, $gambar) {
+    $sql= "INSERT INTO barang (product_id, product_name, stock, price, asal, jenis, expired, tanggal_beli, deskripsi, harga_jual, created_date, updated_date, gambar)VALUES ('$product_id', '$product_name', '$stock', '$price', '$asal', '$jenis', '$expired', '$tanggal_beli', '$deskripsi', '$harga_jual', '$created_date', '$updated_date', '$gambar')";
     $query= $this->db->query($sql);
-    if(!$query)
-    {
+
+    if (!$query) {
       return "Failed";
-    }
-    else
-    {
+    } else {
       return "Success";
     }
   }
-  public function select_barang()
-  {
+
+  public function select_barang() {
     $sql="ambil_barang *  barang";
     $query= $this->db->query($sql);
     return $query;
   }
-  public function edit_barang()
-  {
-    $sql = "SELECT * FROM barang WHERE Product ID = '$product_id'"
-  }  
-  public function delete_barang($product ID)
-  {
-    $sql = "DELETE FROM barang WHERE product ID='$nim'";
-    $query = $this->db->query($sql);
-    if(!$query)
-{
-  return "Failed";
+
+  public function edit_barang() {
+    $sql = "SELECT * FROM barang WHERE Product ID = '$product_id'";
   }
-  else
-{
-return "Success";
+
+  public function delete_barang($product ID) {
+    $sql = "DELETE FROM barang WHERE product ID = '$nim'";
+    $query = $this->db->query($sql);
+
+    if (!$query) {
+      return "Failed";
+    } else {
+      return "Success";
+    }
+  }
 }
+
 ?>
